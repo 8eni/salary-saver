0bd6f8010975254c4d1e354cd575edc8d7a9e236
8eni <bennirussell@gmail.com>
2015-03-26 14:21:41 +0000

    elements added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 
+var totalLeft = 570;
+var daysLeft = 36;
+var dailySpend = (totalLeft / daysLeft);
+document.getElementById("totalLeft").innerHTML = ("€") + (totalLeft);
+document.getElementById("daysLeft").innerHTML = (daysLeft);
+document.getElementById("dailySpend").innerHTML = ("€") + (dailySpend).toFixed(2);
